5fce5aa5817e349cbba3c626089188b5fe97719b
yuliakordunova97 <kordunovayulia@gmail.com>
2023-03-21 12:45:26 +0200

    added files

---
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -0,0 +1,29 @@
+package com.epam.rd.autotasks.matrices;
+import java.util.Arrays;
+
+public class MultiplyMatrix {
+    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+
+        //irregular return. You must write yours
+        return matrix1;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Test your code here!\n");
+
+        // Get a result of your code
+
+        int[][] a = {
+                {0, 12345},
+                {4509, 0},
+                {3, 567} };
+
+        int[][] b = {
+                {653, 0, 25353},
+                {0, 61, 6} };
+
+        int[][] result = multiply(a, b);
+        System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
+    }
+}
